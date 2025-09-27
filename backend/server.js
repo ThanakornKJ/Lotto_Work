7f5c09736c3a5dea2e214e8aa26bab9c722b72df
ThanakornKJ <thanakonkuljit2546@gmail.com>
2025-09-27 19:48:43 +0700

    แก้server.js

--- backend/server.js
+++ backend/server.js
@@ -579,4 +579,5 @@ app.post('/claim-no-prize', async (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
